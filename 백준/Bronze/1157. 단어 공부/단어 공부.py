7aa2462501a70f341f420f7780ef2dec598d04dd
githubryan125 <105639460+githubryan125@users.noreply.github.com>
2024-04-21 20:13:54 +0900

    [Bronze I] Title: 단어 공부, Time: 76 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -3,10 +3,9 @@ word_list = list(set(word))
 
 cnt = []
 for i in word_list:
-  count = word.count
-  cnt.append(count(i))
+  cnt.append(word.count(i))
 
-if cnt.count(max(cnt)) > 1:
+if cnt.count(max(cnt)) > 1: # 가장 많이 사용된 갯수가 같은 알파벳이 2개 이상
   print("?")
 
 else:
