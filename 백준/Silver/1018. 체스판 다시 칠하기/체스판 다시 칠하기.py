2c3c969ab8eacf4254b3cccc25f940c699caff69
githubryan125 <105639460+githubryan125@users.noreply.github.com>
2024-05-01 10:44:50 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,26 +1,28 @@
-N, M = map(int, input().split())
-original = []
-count = []
+# 문제에서 2가지 방법 제시인데 나는 무조건 첫번째 문자를 기준으로만 생각했음
+# 첫번째 문자가 잘못 입력 되었을 경우도 있는것을 간과함
+
+N,M=map(int,input().split())
+original=[]
+count=[]
 
 for _ in range(N):
     original.append(input())
-
 for a in range(N-7):
     for b in range(M-7):
-        index1 = 0
-        index2 = 0
-        for i in range(a, a+8):
-            for j in range(b, b+8):
-                if (i+j) % 2 == 0:
-                    if original[i][j] != 'W':
-                        index1 += 1
-                    if original[i][j] != 'B':
+        index1=0
+        index2=0
+        
+        for i in range(a,a+8):
+            for j in range(b,b+8):
+                if(i+j) %2==0:
+                    if original[i][j] !="W":
+                        index1+=1
+                    if original[i][j] != "B":
                         index2 += 1
                 else:
-                    if original[i][j] != 'B':
-                        index1 += 1
-                    if original[i][j] != 'W':
+                    if original[i][j] !="B":
+                        index1+=1
+                    if original[i][j] != "W":
                         index2 += 1
-        count.append(min(index1, index2))
-
+        count.append(min(index1,index2))
 print(min(count))
