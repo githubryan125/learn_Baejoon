cfbbdcfc81fbb443a8c3977eb1e911837144b109
githubryan125 <105639460+githubryan125@users.noreply.github.com>
2024-05-04 12:46:58 +0900

    [Silver II] Title: 좌표 압축, Time: 1816 ms, Memory: 143264 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.py
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -1,14 +1,21 @@
+# 좌표 압축
+
+# 수직선 위에 n 개의 좌표가 있다
+# Xi를 압축한 Xi' 는 Xi>Xj를 만족하는 서로 다른 좌표  Xj의 개수와 같아야 한다.
+# 좌표압축을 사용한 결과를 출력력
+
+# 제일 작은 수 0
+# 그 다음으로 작은 수 1
+# 결국 수 끼리 순서를 정해주는 ?
+# 처음에 중복제거해서 정렬해줘 그다음에 순서를 대입시켜 그냥
 import sys
 
-n = int(sys.stdin.readline())
-coordinates = list(map(int, sys.stdin.readline().split()))
+n=int(sys.stdin.readline())
+num=list(map(int,sys.stdin.readline().split()))
 
-# 중복 제거 및 정렬
-sorted_coords = sorted(set(coordinates))
+number=sorted(set(num))
 
-# 좌표를 압축하기 위한 딕셔너리 생성
-compress_dict = {coord: index for index, coord in enumerate(sorted_coords)}
+answer={cord: index for index, cord in enumerate(number)}
 
-# 결과 출력
-for coord in coordinates:
-    print(compress_dict[coord], end=" ")
+for cord in num:
+    print(answer[cord], end=" ")
