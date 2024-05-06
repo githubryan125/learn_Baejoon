9798e86227d16614b4c7a4131cf3920d8058b2fe
githubryan125 <105639460+githubryan125@users.noreply.github.com>
2024-05-06 09:58:44 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 240 ms, Memory: 55700 KB -BaekjoonHub

---
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+dict = {}
+
+for i in range(1, n + 1):
+    a = input().rstrip()
+    dict[i] = a
+    dict[a] = i
+
+for i in range(m):
+    quest = input().rstrip()
+    if quest.isalpha():
+        print(dict[quest])
+    else:
+        print(dict[int(quest)])
