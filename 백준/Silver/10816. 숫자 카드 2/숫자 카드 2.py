652a8e3f0fa653f8f75e4b25b259aa338a38588d
githubryan125 <105639460+githubryan125@users.noreply.github.com>
2024-05-06 10:53:21 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 920 ms, Memory: 119748 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,23 +1,19 @@
 import sys
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-n = int(input())
-card = list(map(int, input().split()))
-m = int(input())
-mine = list(map(int, input().split()))
+n=int(input())
+card=list(map(int,input().split()))
+m=int(input())
+mine=list(map(int,input().split()))
 
-card.sort()
-
-dic = {}
-
-for x in card:
-  if x in dic :
-    dic[x] += 1
-  else:
-    dic[x] = 1
-
-for x in mine:
-  if x in dic:
-    print(dic[x], end=' ')
-  else:
-    print('0', end= ' ')
+dit={}
+for i in card:
+    if i in dit:
+        dit[i]+=1
+    else:
+        dit[i]=1
+for i in mine:
+    if i in dit:
+        print(dit[i],end=" ")
+    else:
+        print(0,end=" ")
