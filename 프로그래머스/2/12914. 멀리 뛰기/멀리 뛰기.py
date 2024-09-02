d80d958517d00e09fc75279c0bb8852cf70fd61e
Chan hui Kim <105639460+githubryan125@users.noreply.github.com>
2024-09-02 09:39:02 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.58 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -0,0 +1,11 @@
+def solution(n):
+    
+    jump = [0] * (n+1)
+
+    jump[0] = 1
+    jump[1] = 2
+    
+    for i in range(2,n):
+        jump[i] = jump[i-2] + jump[i-1]
+	
+    return jump[n-1] % 1234567
