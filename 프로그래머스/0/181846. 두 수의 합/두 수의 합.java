9f31018a52357e25ef9f999362baa236878ef7a1
Chan hui Kim <105639460+githubryan125@users.noreply.github.com>
2026-01-05 20:50:08 +0900

    [level 0] Title: 두 수의 합, Time: 17.26 ms, Memory: 81.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = (i >= 0) ? a.charAt(i) - '0' : 0;
+            int y = (j >= 0) ? b.charAt(j) - '0' : 0;
+
+            int sum = x + y + carry;
+            sb.append(sum % 10);
+            carry = sum / 10;
+
+            i--;
+            j--;
+        }
+
+        return sb.reverse().toString();
+    }
+}
